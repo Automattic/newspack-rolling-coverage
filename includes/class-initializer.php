f94afb7247e8f14d362c126847a50ad651b01472
Vrishabhsk <vrishabhsk@gmail.com>
2026-05-25 13:59:23 +0400

    Set initialization sequence

--- includes/class-initializer.php
+++ includes/class-initializer.php
@@ -2,10 +2,15 @@
 /**
  * Newspack Rolling Coverage plugin initialization.
  *
- * @package Newspack
+ * @package Newspack_Rolling_Coverage
  */
 
 namespace Newspack_Rolling_Coverage;
+
+use Newspack_Rolling_Coverage\Core\Post_Type;
+use Newspack_Rolling_Coverage\Core\Taxonomy;
+
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Class to handle the plugin initialization.
@@ -16,6 +21,40 @@ class Initializer {
 	 * Runs the initialization.
 	 */
 	public static function init() {
-		// Register hooks and features here.
+		// Trigger activation/deactivation functionalities.
+		register_activation_hook( NEWSPACK_ROLLING_COVERAGE_PLUGIN_FILE, [ __CLASS__, 'activation_hook' ] );
+		register_deactivation_hook( NEWSPACK_ROLLING_COVERAGE_PLUGIN_FILE, [ __CLASS__, 'deactivation_hook' ] );
+
+		self::includes();
+	}
+
+	/**
+	 * Load feature classes into the WordPress lifecycle.
+	 */
+	private static function includes() {
+		Post_Type::init();
+		Taxonomy::init();
+	}
+
+	/**
+	 * Runs on plugin activation.
+	 */
+	public static function activation_hook() {
+
+		/**
+		 * Fires
+		 */
+		do_action( 'rolling_coverage_activation' );
+	}
+
+	/**
+	 * Runs on plugin deactivation to clean up redundant rewrite rules.
+	 */
+	public static function deactivation_hook() {
+	
+		/**
+		 * Fires when Rolling Coverage plugin is deactivated
+		 */
+		do_action( 'rolling_coverage_deactivation' );
 	}
 }
