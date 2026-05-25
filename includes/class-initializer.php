6e64628ccce17684bfbab16d1197b8c21c2dea09
Vrishabhsk <vrishabhsk@gmail.com>
2026-05-25 13:59:23 +0400

    Set initialization sequence

--- includes/class-initializer.php
+++ includes/class-initializer.php
@@ -31,7 +31,7 @@ class Initializer {
 	private static function includes() {
 		Post_Type::init();
 		Taxonomy::init();
-
+		
 		// Admin interface (only load in admin context).
 		if ( is_admin() ) {
 			Admin::init();
