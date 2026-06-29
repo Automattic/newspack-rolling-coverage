70f9a1826ccef4f812414a3b1681e81d376f571b
Vrishabhsk <vrishabhsk@gmail.com>
2026-06-29 14:40:11 +0400

    Purge deletion implementation

--- src/admin/actions/entry-actions.ts
+++ src/admin/actions/entry-actions.ts
@@ -1,27 +1,21 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { deleteEntry } from '../utils/entry-api';
-import { createDeleteAction } from '../utils/actions';
 import type { Entry, Action, AdminConfig } from '../types';
 
 /**
  * Returns DataViews action definitions for entry rows.
  *
- * @param {AdminConfig} config            Admin config containing edit URLs.
- * @param {() => void}  onActionPerformed Callback invoked after a successful delete to refresh data.
+ * @param {AdminConfig} config Admin config containing edit URLs.
  *
  * @return {Action<Entry>[]} Array of DataViews actions for entries.
  */
-function getEntryActions(
-	config: AdminConfig,
-	onActionPerformed?: () => void
-): Action< Entry >[] {
+function getEntryActions( config: AdminConfig ): Action< Entry >[] {
 	return [
 		{
 			id: 'edit',
@@ -36,22 +30,6 @@ function getEntryActions(
 				}
 			},
 		},
-		createDeleteAction(
-			( id: number ) => deleteEntry( config.restBaseUrls.entries, id ),
-			{
-				singular: __(
-					'Are you sure you want to delete this entry?',
-					'newspack-rolling-coverage'
-				),
-				// translators: %d is the number of entries to delete.
-				plural: __( 'Delete %d entries?', 'newspack-rolling-coverage' ),
-			},
-			{
-				singular: __( 'Delete', 'newspack-rolling-coverage' ),
-				plural: __( 'Delete Entries', 'newspack-rolling-coverage' ),
-			},
-			onActionPerformed
-		),
 	];
 }
 
