1e61a537a4e884963f8779b6388dd904d8b655f8
Miguel Peixe <miguel.peixe@automattic.com>
2026-07-20 11:01:55 -0300

    ci: add PHP test harness and labeler config (#13)

---
+++ tests/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * PHPUnit bootstrap file.
+ *
+ * @package Newspack_Rolling_Coverage
+ */
+
+// Load the Composer autoloader.
+$nrc_autoload = __DIR__ . '/../vendor/autoload.php';
+if ( ! file_exists( $nrc_autoload ) ) {
+	fwrite( STDERR, "Composer autoloader not found. Run `composer install` before running the test suite.\n" ); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_fwrite
+	exit( 1 );
+}
+require_once $nrc_autoload;
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( __DIR__ ) . '/newspack-rolling-coverage.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
