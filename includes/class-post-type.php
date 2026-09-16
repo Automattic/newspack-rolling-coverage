9ed1c84ec689828e6843dd977910cb4b2505c94e
Rishabh Gupta <rishabh.gupta@rtcamp.com>
2026-09-16 23:34:47 +0530

    fix: remove dead reference to undeclared META_PREVIOUS_STATUS constant

--- includes/class-post-type.php
+++ includes/class-post-type.php
@@ -2050,9 +2050,6 @@ class Post_Type {
 			return Archive_Mode::archived_error();
 		}
 
-		$previous_status = get_post_meta( $entry_id, self::META_PREVIOUS_STATUS, true );
-		$previous_status = $previous_status ? $previous_status : 'publish';
-
 		$coverage_id      = 0;
 		$coverage_created = false;
 
